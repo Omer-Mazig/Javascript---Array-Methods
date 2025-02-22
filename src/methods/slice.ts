7ff5b2bef7dfdac1022aba2ab68950bb5dcae9a6
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2025-02-22 17:04:25 +0200

    Update slice.ts

--- src/methods/slice.ts
+++ src/methods/slice.ts
@@ -2,6 +2,11 @@ import { convertToNumber, handleNegativeIndex } from "../arrays";
 
 declare global {
   interface Array<T> {
+    /**
+     * Returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
+     * @param start The beginning index of the specified portion of the array.
+     * @param end The end index of the specified portion of the array.
+     */
     mySlice(start?: number, end?: number): T[];
   }
 }
