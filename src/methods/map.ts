b711ee9ee3bc0daa9f0803f4aa229344aee8f046
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2025-02-22 17:02:14 +0200

    add docs

--- src/methods/map.ts
+++ src/methods/map.ts
@@ -2,6 +2,11 @@ import { constructTypeErrorMessage } from "../arrays";
 
 declare global {
   interface Array<T> {
+    /**
+     * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+     * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+     * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+     */
     myMap<U>(callback: (value: T, index: number, array: T[]) => U): U[];
   }
 }
